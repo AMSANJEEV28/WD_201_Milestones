043b7e64eb79e2404540eb4b5180595f4acc1ae4
AMSANJEEV28 <amsanjeev28@gmail.com>
2022-08-30 14:24:26 +0530

    L2 Milestone

--- index.js
+++ index.js
@@ -1,5 +1,60 @@
-function hello() {
-    console.log("Hello Node.Js! Trying it for the first time");
+const http = require("http");
+const fs = require("fs");
+const readline = require("readline");
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+
+fs.readFile("home.html", function (err, home) {
+  if (err) {
+    throw err;
   }
-  
-  hello();
+  homeContent = home;
+});
+
+fs.readFile("project.html", function (err, project) {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+fs.readFile("registration.html", function (err, registration) {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
+});
+
+const lineDetail = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+http.createServer(function (request, response) {
+  let url = request.url;
+  response.writeHeader(200, { "Content-Type": "text/html" });
+  switch (url) {
+    case "/project":
+      response.write(projectContent);
+      response.end();
+      break;
+    case "/registration":
+      response.write(registrationContent);
+      response.end();
+      break;
+    default:
+      response.write(homeContent);
+      response.end();
+      break;
+  }
+});
+
+lineDetail.question(`Please provide the full file path -`, (path) => {
+  const server = http.createServer(function (req, res) {
+    const stream = fs.createReadStream(`${path}`);
+    stream.pipe(res);
+  });
+  lineDetail.close();
+  server.listen(3000);
+});
